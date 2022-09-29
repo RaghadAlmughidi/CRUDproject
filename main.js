9f174e17d67803460facfc53575592c864ce1cbf
rdmug1421 <ralmughidi@gmail.com>
2022-09-29 14:33:25 +0300

    fun show data &clear inputs are DONE

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ btnCreate.onclick = function() {
         price: price.value,
         taxes: taxes.value,
         ads: ads.value,
+        discount: discount.value,
         btnTotal: btnTotal.innerHTML,
         count: count.value,
         category: category.value,
@@ -48,5 +49,44 @@ btnCreate.onclick = function() {
     }
     dataProduct.push(product) //save obj in list
     localStorage.setItem('products', JSON.stringify(dataProduct)) //storage data when reload never delete----json.stringfy=>convers list to sting brcaus local storage only exccept string
-    console.log(dataProduct)
+    clearData();
+    ShowData();
 }
+
+//clear input
+function clearData() { //clear data after create it
+    title.value = '';
+    price.value = '';
+    taxes.value = '';
+    ads.value = '';
+    discount.value = '';
+    btnTotal.innerHTML = '';
+    count.value = '';
+    category.value = '';
+}
+
+function ShowData() { //show table data by loop in content
+    let table = '';
+    for (let i = 0; i < dataProduct.length; i++) {
+        table +=
+            `<tr>
+            <td>${i}</td>
+            <td>${dataProduct[i].title}</td>
+            <td>${dataProduct[i].price}</td>
+            <td>${dataProduct[i].taxes}</td>
+            <td>${dataProduct[i].ads}</td>
+            <td>${dataProduct[i].discount}</td>
+            <td>${dataProduct[i].btnTotal}</td>
+            <td>${dataProduct[i].category}</td>
+            <td><button id="update" class="btn">Update</button></td>
+            <td><button id="delete" class="btn">Delete</button></td>
+        </tr>`
+    }
+    document.getElementById('tbody').innerHTML = table;
+
+}
+
+
+
+
+ShowData();
